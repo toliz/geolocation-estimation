a7b04bbaed3fa13a2fc1a7365ee8c248f9b52ed2
toliz <ap.panagiotopoulos@gmail.com>
2021-01-09 11:20:41 +0200

    Removed copied code

--- src/models/MultiPartitioningClassifier.py
+++ src/models/MultiPartitioningClassifier.py
@@ -38,11 +38,6 @@ class MultiPartitioningClassifier(LightningModule):
             ]
         )
 
-        # Load pretrained
-        """if self.hparams.pretrained:
-            logger.info('Load weights from pre-trained model')
-            load_weights(self, self.hparams.pretrained)"""
-
     def forward(self, x):
         features = self.backbone(x)
         preds = [classifier(features) for classifier in self.classifiers]
@@ -63,64 +58,10 @@ class MultiPartitioningClassifier(LightningModule):
         losses = [F.cross_entropy(pred, target) for pred, target in zip(preds, targets)]
         loss = sum(losses)
 
-        """# log top-k accuracy for each partitioning
-        individual_accuracy_dict = utils_global.accuracy(
-            output, target, [p.shortname for p in self.partitionings]
-        )
-        
-        # log loss for each partitioning
-        individual_loss_dict = {
-            f'loss_val/{p}': l
-            for (p, l) in zip([p.shortname for p in self.partitionings], losses)
-        }
-
-        # log GCD error@km threshold
-        distances_dict = {}
-
-        if self.hierarchy is not None:
-            hierarchy_logits = [
-                yhat[:, self.hierarchy.M[:, i]] for i, yhat in enumerate(output)
-            ]
-            hierarchy_logits = torch.stack(hierarchy_logits, dim=-1,)
-            hierarchy_preds = torch.prod(hierarchy_logits, dim=-1)
-
-        pnames = [p.shortname for p in self.partitionings]
-        if self.hierarchy is not None:
-            pnames.append('hierarchy')
-        for i, pname in enumerate(pnames):
-            # get predicted coordinates
-            if i == len(self.partitionings):
-                i = i - 1
-                pred_class_indexes = torch.argmax(hierarchy_preds, dim=1)
-            else:
-                pred_class_indexes = torch.argmax(output[i], dim=1)
-            pred_latlngs = [
-                self.partitionings[i].get_lat_lng(idx)
-                for idx in pred_class_indexes.tolist()
-            ]
-            pred_lats, pred_lngs = map(list, zip(*pred_latlngs))
-            pred_lats = torch.tensor(pred_lats, dtype=torch.float)
-            pred_lngs = torch.tensor(pred_lngs, dtype=torch.float)
-            # calculate error
-            distances = utils_global.vectorized_gc_distance(
-                pred_lats,
-                pred_lngs,
-                true_lats.type_as(pred_lats),
-                true_lngs.type_as(pred_lats),
-            )
-            distances_dict[f'gcd_{pname}_val'] = distances
-
-        output = {
-            'loss_val/total': loss,
-            **individual_accuracy_dict,
-            **individual_loss_dict,
-            **distances_dict,
-        }
-        return output"""
         return loss
 
     def configure_optimizers(self):
-        optimizer = SGD(self.parameters(), lr=0.01)#, **self.hparams.optim['params'])
-        scheduler = lr_scheduler.MultiStepLR(optimizer, gamma=0.5, milestones=[4, 8, 12, 13, 14, 15])#, **self.hparams.scheduler['params'])
+        optimizer = SGD(self.parameters(), lr=0.01
+        scheduler = lr_scheduler.MultiStepLR(optimizer, gamma=0.5, milestones=[4, 8, 12, 13, 14, 15])
 
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
