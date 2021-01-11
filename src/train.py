199f82a0e8b9a53710e64cf6943c78374fd32b21
toliz <ap.panagiotopoulos@gmail.com>
2021-01-11 13:15:36 +0200

    Updated config file format

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 from pathlib import Path
+import yaml
+import logging
 
 from models import MultiPartitioningClassifier
 from data import GeoDataModule
@@ -9,8 +11,12 @@ from pytorch_lightning import Trainer
 def parse_args():
     args = ArgumentParser()
 
-    args.add_argument("-c", "--config", type=Path, default=Path("config/baseM.yml"))
-    args.add_argument("--progbar", action="store_true")
+    args.add_argument(
+        "-m", "--model-dir",
+        type=Path,
+        default=Path("models/baseM"),
+        help="Path to model folder",
+    )
     
     return args.parse_args()
 
@@ -18,15 +24,24 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
+    logging.basicConfig(
+        format='%(asctime)s [%(levelname)s]: %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S',
+        level=logging.INFO,
+    )
+
+    with open(args.model_dir / 'config.yml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        model_params = config["model_params"]
+        trainer_params = config["trainer_params"]
+
     # Init model & data module
-    model = MultiPartitioningClassifier({})
+    model = MultiPartitioningClassifier(model_params)
     datamodule = GeoDataModule()
 
     # Init Trainer
     trainer = Trainer(
-        gpus=1,
-        max_epochs=15,
-        precision=16,
+        **trainer_params,
         progress_bar_refresh_rate=1,
     )
 
