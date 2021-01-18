1491b2a0f9092d05d4b87a2aeb6ad9d3c4f9a033
toliz <ap.panagiotopoulos@gmail.com>
2021-01-18 10:11:47 +0200

    Major update - added test test code as well

--- src/models/MultiPartitioningClassifier.py
+++ src/models/MultiPartitioningClassifier.py
@@ -19,13 +19,16 @@ class MultiPartitioningClassifier(LightningModule):
         load_pretrained = False,
     ):
         super().__init__()
+        self.name = name
 
         if type(cell_dir) is not Path:
             cell_dir = Path(cell_dir)
 
         # Build hierarchy
         self.partitionings = [utils.cells.Partitioning(pfile) for pfile in cell_dir.iterdir()]
+
         if load_hierarchy:
+            list.sort(self.partitionings, key=len)
             self.hierarchy = pickle.load(open(f'models/{name}/hierarchy.pkl', 'rb'))
         else:
             self.hierarchy = utils.cells.Hierarchy(self.partitionings)
@@ -50,55 +53,120 @@ class MultiPartitioningClassifier(LightningModule):
 
         # Load weights
         if load_pretrained:
-            self.load(f'models/{name}/pretrained.ckpt')
+            self.load_state_dict(torch.load('models/baseM/weights.pt'))
 
-    def load(self, pretrained_path: str):
-        checkpoint = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    def forward(self, images):
+        assert isinstance(images, torch.Tensor)
+        assert (images.ndim == 5) # batch, crops, channels, height, width
+        
+        # Reshape crop dimension to batch
+        batch_size, num_crops = images.shape[:2]
+        images = torch.reshape(images, (batch_size * num_crops, *images.shape[2:]))
 
-        state_dict_features = OrderedDict()
-        state_dict_classifier = OrderedDict()
-        for k, w in checkpoint['state_dict'].items():
-            if k.startswith('model'):
-                state_dict_features[k.replace('model.', '')] = w
-            elif k.startswith('classifier'):
-                state_dict_classifier[k.replace('classifier.', '')] = w
-            else:
-                logging.warning(f'Unexpected prefix in state_dict: {k}')
-        self.backbone.load_state_dict(state_dict_features, strict=True)
-        self.classifiers.load_state_dict(state_dict_classifier, strict=True)
-
-
-    def forward(self, x):
-        features = self.backbone(x)
+        # Forward pass
+        features = self.backbone(images)
         preds = [classifier(features) for classifier in self.classifiers]
+        preds = [F.softmax(pred, dim=1) for pred in preds]
+
+        # Respape back to access individual crops
+        preds = [
+            torch.reshape(pred, (batch_size, num_crops, *list(pred.shape[1:]))) for pred in preds
+        ]
+
+        # Calculate max over crops
+        preds = [torch.max(pred, dim=1)[0] for pred in preds]
+
+        # Hierarchical prediction
+        hierarchy_logits = torch.stack(
+            [pred[:, self.hierarchy[i]] for i, pred in enumerate(preds)],
+            dim=-1,
+        )
+        hierarchy_preds = torch.prod(hierarchy_logits, dim=-1)
+
+        # Get latitude & longitute
+        for i, prob in enumerate(preds):
+            preds[i] = torch.argmax(prob, dim=1)
+            preds[i] = [self.partitionings[i].get_coords(c.item()) for c in preds[i]]
+
+        hierarchy_preds = torch.argmax(prob, dim=1)
+        hierarchy_preds = [self.partitionings[i].get_coords(c.item()) for c in hierarchy_preds]
+        
+        preds = dict(zip([p.name for p in self.partitionings], preds))
+        preds['hierarchy'] = hierarchy_preds
 
         return preds
 
     def configure_optimizers(self):
-        optimizer = torch.optim.__dict__[self.hparams['optimizer']['name']](
+        optimizer = torch.optim.SGD(
             self.parameters(),
-            **self.hparams['optimizer']['params']
+            lr=0.01,
+            momentum=0.9,
+            weight_decay=0.0001,
         )
         
-        scheduler = torch.optim.lr_scheduler.__dict__[self.hparams['scheduler']['name']](
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
             optimizer,
-            **self.hparams['scheduler']['params']
+            gamma=0.5,
+            milestones=[4, 8, 12, 13, 14, 15],
         )
 
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
 
     def training_step(self, batch, batch_idx):
-        images, targets = batch
-        preds = self(images)
+        images, targets, _, _ = batch
+
+        # Forward pass
+        features = self.backbone(images)
+        preds = [classifier(features) for classifier in self.classifiers]
+
+        # Loss
         losses = [F.cross_entropy(pred, target) for pred, target in zip(preds, targets)]
         loss = sum(losses)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
-        images, targets = batch
-        preds = self(images)
+        images, targets, _, _ = batch
+
+        # Forward pass
+        features = self.backbone(images)
+        preds = [classifier(features) for classifier in self.classifiers]
+
+        # Loss
         losses = [F.cross_entropy(pred, target) for pred, target in zip(preds, targets)]
         loss = sum(losses)
 
         return loss
+
+    def test_step(self, batch, batch_idx, dataloader_idx = None):
+        images, _, true_lat, true_lng = batch
+        pred_coords = self(images)
+
+        GCD = {}
+        for pname, pred_coord in pred_coords.items():
+            pred_lat, pred_lng = zip(*pred_coord)
+
+            pred_lat = torch.tensor(pred_lat, dtype=torch.float, device=true_lat.device)
+            pred_lng = torch.tensor(pred_lng, dtype=torch.float, device=true_lng.device)
+
+            GCD[pname] = utils.report.GCD(pred_lat, pred_lng, true_lat, true_lng)
+
+        return GCD
+
+    def test_epoch_end(self, outputs):
+        results = []
+
+        if not isinstance(outputs[0], list):
+            outputs = [outputs]
+
+        pnames = [partitioning.name for partitioning in self.partitionings]
+        for output in outputs:
+            accuracies = {}
+
+            for pname in pnames:
+                GCD = torch.cat([o[pname] for o in output], dim=0)
+                accuracies[pname] = utils.report.acc(GCD)
+
+            results.append(accuracies)
+        
+        pickle.dump(results, open(f'models/{self.name}/test_report.pkl', 'wb'))
